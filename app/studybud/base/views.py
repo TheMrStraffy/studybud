a9741355333ef84f7a9d44502a4d3a552aba0b5b
DIGIMOBIL\carmelo.miuccio <carmelo.miuccio@digimobil.it>
2025-05-12 14:24:21 +0200

    login, logout

--- app/studybud/base/views.py
+++ app/studybud/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpRequest
 from .models import Room, Topic
 from .forms import RoomForm
@@ -12,13 +15,41 @@ from .forms import RoomForm
 #     {'id' : 3, 'name' : 'lets learn C++!'},
 # ]
 
+def loginView(request:HttpRequest):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
+    context = {
+
+    }
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request:HttpRequest):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     # With Q imported from django.db.models we can set more parameters to search with
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
-        Q(name__icontains=q)
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
         ) 
     
     topics = Topic.objects.all()
